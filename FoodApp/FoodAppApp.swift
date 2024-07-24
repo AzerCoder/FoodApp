a081269f10d51d0e7ff487fd0ada35fef46a5944
A'zamjon Abdumuxtorov <azamjonabdumuxtorov1220@gmail.com>
2024-07-24 10:34:08 +0500

    Initial Commit

---
+++ FoodApp/FoodAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodAppApp.swift
+//  FoodApp
+//
+//  Created by A'zamjon Abdumuxtorov on 24/07/24.
+//
+
+import SwiftUI
+
+@main
+struct FoodAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
